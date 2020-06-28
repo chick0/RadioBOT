6643b3d68b15bda02bf71c1406dee3f5c984652b
chick <chick_0@ch0.kr>
2020-06-28 21:28:39 +0900

    1.6.1 - logger updated

--- data/lib/log.py
+++ data/lib/log.py
@@ -7,25 +7,24 @@ import logging
 logger = logging.getLogger()
 
 
-def __clean_old_log(boot_time):
-    log_files = os.listdir("./log")
-    if len(log_files) > 1:
-        logger.info("Moving Old Log...")
-        try:
-            for item in log_files:
-                if item != f"{boot_time}.log":
-                    logger.info(f"old_log: {item}")
-                    os.rename(f"./log/{item}", f"./log/old/{item}")
-                    logger.info(f"OK! - Old log moved -> {item}")
-        except FileNotFoundError:
-            os.mkdir("./log/old/")
-            for item in log_files:
-                if item != f"{boot_time}.log":
-                    logger.info(f"old_log: {item}")
-                    os.rename(f"./log/{item}", f"./log/old/{item}")
-                    logger.info(f"OK! - Old log moved -> {item}")
-        except Exception as e:
-            logger.error(f"FAIL - {e}")
+def create_file_handler(log_formatter):
+    boot_time = time.strftime("%Y-%m-%d %HH %MM", time.localtime(time.time()))
+    try:
+        file_handler = logging.FileHandler(f"log/{boot_time}.log")
+        file_handler.setFormatter(log_formatter)
+    except FileNotFoundError:
+        os.mkdir("log/")
+        file_handler = logging.FileHandler(f"log/{boot_time}.log")
+        file_handler.setFormatter(log_formatter)
+
+    return file_handler
+
+
+def create_console_handler(log_formatter):
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+
+    return console_handler
 
 
 def create_logger():
@@ -35,23 +34,5 @@ def create_logger():
     )
 
     logger.setLevel(logging.INFO)
-
-    boot_time = time.strftime("%Y-%m-%d %HH %MM", time.localtime(time.time()))
-    try:
-        file_handler = logging.FileHandler(f"log/{boot_time}.log")
-        file_handler.setFormatter(log_formatter)
-        logger.addHandler(file_handler)
-    except FileNotFoundError:
-        os.mkdir("log/")
-        file_handler = logging.FileHandler(f"log/{boot_time}.log")
-        file_handler.setFormatter(log_formatter)
-        logger.addHandler(file_handler)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_formatter)
-    logger.addHandler(console_handler)
-
-    del console_handler
-    del file_handler
-
-    __clean_old_log(boot_time)
+    logger.addHandler(create_console_handler(log_formatter))
+    logger.addHandler(create_file_handler(log_formatter))
